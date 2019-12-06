4f5bdd63fb01a3baf9c48a094f8f82af638b3e57
Fabrizio Veneziano <fabrizioveneziano@yahoo.it>
2019-12-06 09:15:35 +0100

    Added Moto

---
+++ src/concessionario/core/Moto.java
@@ -0,0 +1,22 @@
+package concessionario.core;
+
+public class Moto extends Veicolo {
+	private Double altezzaSedile;
+
+	public Moto(String targa, String produttore, String modello, String alimentazione, String colore, Double prezzo, Double altezzaSedile) {
+		super(targa, produttore, modello, alimentazione, colore, prezzo);
+		this.altezzaSedile=altezzaSedile;
+		
+	}
+
+	public Double getAltezzaSedile() {
+		return altezzaSedile;
+	}
+	
+	public void Visualizza() {
+		System.out.println("MOTO ");
+		super.Visualizza();
+		System.out.println("Con un'altezza del sedile pari a " + getAltezzaSedile());
+		
+	}
+}
