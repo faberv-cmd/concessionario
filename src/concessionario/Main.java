4142e8f06cffbfdb7dc3288215e92fe4d684c0f2
Alberto Brusa <gin.brusa@hotmail.it>
2019-12-06 17:13:47 +0100

    Modified main

--- src/concessionario/Main.java
+++ src/concessionario/Main.java
@@ -5,7 +5,9 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import concessionario.core.Auto;
 import concessionario.core.Dipendente;
+import concessionario.core.Moto;
 import concessionario.core.Veicolo;
 import concessionario.database.DriverDiskVeicolo;
 import concessionario.database.DriverInterface;
@@ -28,7 +30,6 @@ public class Main {
 		
 		tabellaDipendente.insert(new Dipendente(1, "Mario", "Rossi",true));
 		tabellaDipendente.insert(new Dipendente(10, "Mario", "Rossi",false));
-		
 		tabellaDipendente.insert(new Dipendente(2, "Mario", "Rossi",false));
 		tabellaDipendente.insert(new Dipendente(43, "Luca", "verdi", true));
 		tabellaDipendente.insert(new Dipendente(45, "Luca", "verdi", false));
@@ -36,32 +37,36 @@ public class Main {
 		tabellaDipendente.delete(10);
 		tabellaDipendente.update(tabellaDipendente.get(2));
 		tabellaDipendente.update(tabellaDipendente.get(45));
-		tabellaDipendente.update(tabellaDipendente.get(43));
-		tabellaDipendente.update(tabellaDipendente.get(50));
-		tabellaDipendente.update(tabellaDipendente.get(10));
-		
-		//tabellaVeicolo.insert(new )
-		
-		//System.out.println(driverVeic.read());
-		
-		//driverVeic.write(driverVeic.read());
-		
-		//tabellaVeicolo.insert(new Veicolo())
 		
 		
+	    driverVeic.read();
+		
+		tabellaVeicolo.insert(new Auto("uuu","mercedes","classe a","benzina","blu",34000.0,3));
+		tabellaVeicolo.insert(new Moto("VIVA","mercedes","classe a","benzina","blu",34000.0,33.3));
+		
+		tabellaVeicolo.delete("uuu");
+		
+		// Per poter applizare l'update
+		tabellaVeicolo.get("FAFA");
+		Veicolo temp = null;
+		temp = tabellaVeicolo.get("FAFA");
+		temp.setPrezzo(300.0);
+		tabellaVeicolo.update(temp); //FUNZIONAAAAAA
+		
+		System.out.println(temp.toString());
+	
+		
+		/*
 		Iterator i = driverVeic.read().iterator();
 		
 		while(i.hasNext()) {
 			System.out.println(i.next());
 		}
-		
-		
+		*/
 		
 		driverVeic.write(driverVeic.read());
 		
 		
-		
-		//System.out.println(tabellaDipendente.get());
 		
 		for(Dipendente d : tabellaDipendente.get()) {
 			if(d.getAmministratore()==true) {
